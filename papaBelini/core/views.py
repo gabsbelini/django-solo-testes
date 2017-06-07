d817dc51a0c380e9b454fe10f79bf3e32282ec95
Gabriel Belini <gabs_belini@hotmail.com>
2017-06-07 13:58:37 -0300

    added plot

--- papaBelini/core/views.py
+++ papaBelini/core/views.py
@@ -1,5 +1,6 @@
 import urllib.request
 from bs4 import BeautifulSoup
+import datetime
 
 from django.shortcuts import render
 from django.views.generic import (
@@ -11,38 +12,42 @@ from django.contrib.auth.forms import PasswordChangeForm
 import plotly.offline as opy
 import plotly.graph_objs as go
 
-class Graph(TemplateView):
-    template_name = 'graph.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(Graph, self).get_context_data(**kwargs)
-
-        x = [-2, 0, 4, 6, 7]
-        y = [q**2-q+3 for q in x]
-        trace1 = go.Scatter(x=x, y=y, marker={'color': 'red', 'symbol': 104, 'size': "10"},
-                            mode="lines",  name='1st Trace')
-        data=go.Data([trace1])
-        layout=go.Layout(title="Meine Daten", xaxis={'title':'x1'}, yaxis={'title':'x2'})
-        figure=go.Figure(data=data,layout=layout)
-        div = opy.plot(figure, auto_open=False, output_type='div')
-
-        context['graph'] = div
-
-        return context
-
-
-
 
 class CafeView(TemplateView):
     def get_context_data(self, **kwargs):
         class AppURLopener(urllib.request.FancyURLopener):
             version = "Mozilla/5.0"
+
         opener = AppURLopener()
         response = opener.open('https://www.investing.com/commodities/us-coffee-c-contracts').read()
         soup = BeautifulSoup(response, 'html.parser')
-        price = soup.find("td", {"class": "pid-8832-last"}).get_text()
+        price = float(soup.find("td", {"class": "pid-8832-last"}).get_text())
         context = super(CafeView, self).get_context_data(**kwargs)
         context['preco'] = price
+        try:
+            lista_prices
+        except NameError:
+            lista_prices = []
+        else:
+            lista_prices.append(price)
+        try:
+            lista_hours
+        except NameError:
+            lista_hours = []
+        else:
+            lista = str(datetime.datetime.now())
+            hora, ponto, ms = lista.split()[1].partition('.')
+            lista_hours.append(hora)
+        x = [-2.5, 0.4, 4.2, 6.9, 7.8, 8, 10, 15, 25]
+        y = [q**2-q+3 for q in x]
+        trace1 = go.Scatter(x=lista_hours, y=lista_prices, marker={'color': 'red', 'symbol': 104, 'size': "10"},
+                            mode="lines",  name='1st Trace')
+        data=go.Data([trace1])
+        layout=go.Layout(title="Coffee Price", xaxis={'title':'x1'}, yaxis={'title':'x2'})
+        figure=go.Figure(data=data,layout=layout)
+        div = opy.plot(figure, auto_open=False, output_type='div')
+
+        context['graph'] = div
         return context
     template_name = 'cafe.html'
 
@@ -81,4 +86,3 @@ sobre = SobreView.as_view()
 criar_conta = CriarContaView.as_view()
 canal = CanalView.as_view()
 cafe = CafeView.as_view()
-graph = Graph.as_view()
--- papaBelini/papaBelini/urls.py
+++ papaBelini/papaBelini/urls.py
@@ -22,7 +22,6 @@ from core import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'cafe$', views.cafe, name='cafe'),
-    url(r'graph$', views.graph, name='graph'),
     url(r'^contato/$', views.contato, name='contato'),
     url(r'^sobre$', views.sobre, name='sobre'),
     url(r'^comecando$', views.comecando, name='comecando'),
